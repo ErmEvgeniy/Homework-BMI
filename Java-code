ab46b3e8b250a1f950f814749c3c00ceb259546f
Erm Evgeniy <erm.evgeniy18@gmail.com>
2020-04-15 19:47:22 +0300

    Second commit

--- Java-code
+++ Java-code
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService bmiService = new BmiService();
 
-        double Bmi = bmiService.calculate(70,  1.7);
+        double Bmi = bmiService.calculate(49,  1.56);
 
         System.out.println(Bmi);
     }
--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
     public double calculate(int length, double mass) {
-        int weight = 70;
-        double height = 1.7;
-        double Bmi = weight / (height * 2);
+        int weight = 49;
+        double height = 1.56;
+        double Bmi = weight / (height*height);
         {
             return Bmi;
         }
